6e5667997c60b781efbf4f55e06d2f85766ca8f7
abdelrahman-Ebrahim <abdelrahmanebrabima@gmail.com>
2024-12-17 16:33:03 +0200

    Join Overlay

--- app/components/JoinUs/JoinUs.tsx
+++ app/components/JoinUs/JoinUs.tsx
@@ -3,17 +3,23 @@ import React from "react";
 const JoinUs = () => {
   return (
     <section className="mt-16 mx-[112px] min-h-[350px] rounded-xl bg-center bg-cover bg-joinBg font-rubik relative mb-96">
-      <div className="pt-9 pr-[66px] flex flex-col gap-4 text-white">
-        <h2 className="font-extrabold text-[44px]">
+      {/* Overlay */}
+      <div className="absolute inset-0 rounded-xl bg-[rgba(0,0,0,0.3)] z-0"></div>
+
+      {/* Content Wrapper */}
+      <div className="relative z-10 pt-9 pr-[66px] flex flex-col gap-4 text-white">
+        <h2 className="font-extrabold text-[44px] leading-snug">
           انضم إلى .... <br /> أكـــــبر مجتمع فني <br /> في الوطن العربي
         </h2>
-        <p className="text-[22px] font-normal">
+        <p className="text-[22px] font-normal leading-relaxed">
           استكشف الفن، وطور مهاراتك كفنان، أو قم بشراء وبيع الأعمال يمكنك <br />
           القيام بكل ذلك هنا.
         </p>
       </div>
-      <div className="absolute bottom-5 left-16">
-        <button className="bg-white flex items-center justify-center rounded-full py-[18px] px-[45px] font-semibold text-lg">
+
+      {/* Button */}
+      <div className="absolute bottom-5 left-16 z-10">
+        <button className="bg-white text-black flex items-center justify-center rounded-full py-[18px] px-[45px] font-semibold text-lg">
           سجل يا فنان
         </button>
       </div>
