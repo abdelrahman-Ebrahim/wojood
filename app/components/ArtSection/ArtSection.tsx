3c11bdedbb3c1ab3062138abf2b1c2c949e4a069
abdelrahman-Ebrahim <abdelrahmanebrabima@gmail.com>
2024-12-18 14:33:33 +0200

    art skew img

--- app/components/ArtSection/ArtSection.tsx
+++ app/components/ArtSection/ArtSection.tsx
@@ -4,20 +4,26 @@ import React from "react";
 const ArtSection = () => {
   return (
     <section className="mt-16 text-center font-rubik">
-      <div className="flex gap-[127px] justify-center items-center pb-[62px]">
+      <div className="flex gap-[127px] justify-center items-center py-[92px]">
         {/* Image Section */}
         <div className="mr-[137px] relative w-full flex justify-center items-center">
-        
-          <div className="relative w-full flex justify-center items-center">
-            <Image
-              src="/art1.png"
-              alt="art1"
-              width={473}
-              height={330}
-              className="-rotate-6 shadow-artCombinedShadows"
-            />
-            <div className="absolute bg-[#FF4800] w-[651px] h-[100px] top-[15%] left-[50px] -z-10 overflow-hidden skew-x-0 -skew-y-2" />
-            <div className="absolute bg-[#FF4800] w-[651px] h-[100px] top-[60%] left-[50px] -z-10 overflow-hidden skew-x-0 skew-y-2" />
+          <div className="w-full">
+            <div className="flex flex-col justify-center items-center gap-[44px] relative">
+              {/* Two Skewed Orange Lines */}
+              <div className="bg-[#FF4800] w-[651px] h-[100px] skew-x-0 skew-y-2" />
+              <div className="bg-[#FF4800] w-[651px] h-[100px] skew-x-0 -skew-y-2" />
+
+              {/* Centered Image */}
+              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-[60%] w-full flex justify-center items-center">
+                <Image
+                  src="/art1.png"
+                  alt="art1"
+                  width={473}
+                  height={350}
+                  className="-rotate-6 shadow-artCombinedShadows w-[473px] h-auto"
+                />
+              </div>
+            </div>
           </div>
         </div>
         {/* Text Section */}
