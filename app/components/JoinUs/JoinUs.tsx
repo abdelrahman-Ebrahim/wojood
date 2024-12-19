8bca2fc96122e12252048dd1611e5f2aaf519181
abdelrahman-Ebrahim <abdelrahmanebrabima@gmail.com>
2024-12-19 14:48:47 +0200

    join i18n 1st

--- app/components/JoinUs/JoinUs.tsx
+++ app/components/JoinUs/JoinUs.tsx
@@ -1,8 +1,11 @@
 "use client";
 import React from "react";
 import { motion } from "framer-motion";
+import { useLocale, useTranslations } from "next-intl";
 
 const JoinUs = () => {
+  const t = useTranslations("Join");
+  const locale = useLocale();
   return (
     <section className="mt-16 mx-[112px] min-h-[350px] rounded-xl bg-center bg-cover bg-joinBg font-rubik relative">
       {/* Overlay with animation */}
@@ -14,7 +17,11 @@ const JoinUs = () => {
       ></motion.div>
 
       {/* Content Wrapper */}
-      <div className="relative z-10 pt-9 pr-[66px] flex flex-col gap-4 text-white">
+      <div
+        className={`relative z-10 pt-9 ${
+          locale === "ar" ? "pr-[66px]" : "pl-[66px]"
+        } flex flex-col gap-4 text-white`}
+      >
         {/* Heading with animation */}
         <motion.h2
           className="font-extrabold text-[44px] leading-snug"
@@ -22,7 +29,8 @@ const JoinUs = () => {
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 1, ease: "easeOut" }}
         >
-          انضم إلى .... <br /> أكـــــبر مجتمع فني <br /> في الوطن العربي
+          {t("header1")} <br /> {t("header2")}
+          <br /> {t("header3")}
         </motion.h2>
 
         {/* Paragraph with animation */}
@@ -32,14 +40,17 @@ const JoinUs = () => {
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 1.2, ease: "easeOut" }}
         >
-          استكشف الفن، وطور مهاراتك كفنان، أو قم بشراء وبيع الأعمال يمكنك <br />
-          القيام بكل ذلك هنا.
+          {t("des1")}
+          <br />
+          {t("des2")}
         </motion.p>
       </div>
 
       {/* Button with animation */}
       <motion.div
-        className="absolute bottom-5 left-16 z-10"
+        className={`absolute ${
+          locale === "ar" ? "left-16" : "right-16"
+        } bottom-5 z-10`}
         initial={{ opacity: 0, y: 20 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{
@@ -50,7 +61,7 @@ const JoinUs = () => {
         }}
       >
         <button className="bg-white text-black flex items-center justify-center rounded-full py-[18px] px-[45px] font-semibold text-lg">
-          سجل يا فنان
+          {t("button")}
         </button>
       </motion.div>
     </section>
