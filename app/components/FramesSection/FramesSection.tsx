dee386d75e313f3f7401d351646f94cfcbd0448a
abdelrahman-Ebrahim <abdelrahmanebrabima@gmail.com>
2024-12-19 14:32:50 +0200

    frames section i18n

--- app/components/FramesSection/FramesSection.tsx
+++ app/components/FramesSection/FramesSection.tsx
@@ -2,11 +2,14 @@
 import Image from "next/image";
 import React from "react";
 import { motion } from "framer-motion";
+import { useLocale, useTranslations } from "next-intl";
 
 const FramesSection = () => {
+  const t = useTranslations("Frames");
+  const locale = useLocale();
   return (
     <section className="mt-16 text-center font-rubik">
-      <div className="flex justify-center items-center gap-[80px]">
+      <div className={`flex justify-center items-center ${locale === "ar" ? "gap-[80px]" : "gap-[200px]"}`}>
         {/* Text Section */}
         <motion.div
           className="flex flex-col min-w-[364px]"
@@ -29,7 +32,7 @@ const FramesSection = () => {
             <h2 className="text-[40px] font-semibold relative inline-block leading-3">
               <span className="relative">
                 <span className="absolute bottom-1 -left-[18px] w-[calc(100%+20px)] h-[24px] bg-[#3AFAFC] -z-10" />
-                لوحات نابضــة
+                {t("title1")}
               </span>
             </h2>
           </div>
@@ -39,7 +42,7 @@ const FramesSection = () => {
             <h2 className="text-[40px] font-semibold relative inline-block">
               <span className="relative">
                 <span className="absolute bottom-1 -left-[18px] w-[calc(100%+20px)] h-[24px] bg-[#3AFAFC] -z-10" />
-                تحـــكى قصصاََ
+                {t("title2")}
               </span>
             </h2>
           </div>
@@ -55,8 +58,8 @@ const FramesSection = () => {
               ease: "easeOut",
             }}
           >
-            اكتشف القطع التي تروي حكايتك <br />
-            وتضفي على منزلك لمسة فنية تعبر <br /> عنك.
+            {t("subtitle1")} <br />
+            {t("subtitle2")} <br /> {t("subtitle3")}
           </motion.p>
         </motion.div>
 
