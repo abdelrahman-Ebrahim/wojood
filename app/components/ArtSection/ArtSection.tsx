47cc0e4359e1d0910de7f1752b05b9bf209ebfaf
abdelrahman-Ebrahim <abdelrahmanebrabima@gmail.com>
2024-12-21 16:27:31 +0200

    resp artSection

--- app/components/ArtSection/ArtSection.tsx
+++ app/components/ArtSection/ArtSection.tsx
@@ -9,11 +9,11 @@ const ArtSection = () => {
   const locale = useLocale();
   return (
     <section className="mt-16 text-center font-rubik">
-      <div className="flex gap-[127px] justify-center items-center py-[92px]">
+      <div className="flex flex-col lg:flex-row gap-[60px] md:gap-[127px] justify-center items-center py-10 md:py-[92px]">
         {/* Image Section */}
         <motion.div
           className={`${
-            locale === "ar" ? "mr-[137px]" : "ml-[137px]"
+            locale === "ar" ? "lg:mr-[200px] xl:mr-[137px]" : "lg:ml-[200px] xl:ml-[137px]"
           }  relative w-full flex justify-center items-center`}
           initial={{ opacity: 0, y: 100 }} // Start with opacity 0 and below
           whileInView={{ opacity: 1, y: 0 }} // whileInView to full opacity and original Y position
@@ -21,14 +21,14 @@ const ArtSection = () => {
         >
           <div className="w-full">
             <motion.div
-              className="flex flex-col justify-center items-center gap-[44px] relative"
+              className="flex flex-col justify-center items-center gap-[44px] relative sm:mx-0"
               initial={{ opacity: 0, scale: 0.8 }} // Initial state
               whileInView={{ opacity: 1, scale: 1 }} // Target state
               transition={{ duration: 1, ease: "easeInOut" }} // Smooth animation
             >
               {/* Two Skewed Orange Lines */}
-              <div className="bg-[#FF4800] w-[651px] h-[100px] skew-x-0 skew-y-2" />
-              <div className="bg-[#FF4800] w-[651px] h-[100px] skew-x-0 -skew-y-2" />
+              <div className="bg-[#FF4800] w-[320px] sm:w-[400px] lg:w-[500px] xl:w-[651px] h-[100px] skew-x-0 skew-y-2" />
+              <div className="bg-[#FF4800] w-[320px] sm:w-[400px] lg:w-[500px] xl:w-[651px] h-[100px] skew-x-0 -skew-y-2" />
 
               {/* Centered Image */}
               <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-[60%] w-full flex justify-center items-center">
@@ -37,7 +37,7 @@ const ArtSection = () => {
                   alt="art1"
                   width={473}
                   height={350}
-                  className="-rotate-6 shadow-artCombinedShadows w-[473px] h-auto"
+                  className="-rotate-6 shadow-artCombinedShadows w-[300px] sm:w-[360px] lg:w-[400px] xl:w-[473px] h-auto"
                 />
               </div>
             </motion.div>
@@ -46,17 +46,17 @@ const ArtSection = () => {
         {/* Text Section */}
         <div
           className={`flex flex-col min-w-[364px] ${
-            locale === "ar" ? "ml-[127px]" : "mr-[127px]"
+            locale === "ar" ? "lg:ml-[200px] xl:ml-[127px]" : "lg:mr-[200px] xl:mr-[127px]"
           }`}
         >
           {/* Text Block 1 */}
           <motion.div
-            className="text-start"
+            className="text-center lg:text-start"
             initial={{ opacity: 0, x: -100 }} // Start from left
             whileInView={{ opacity: 1, x: 0 }} // whileInView to original position
             transition={{ duration: 1, ease: "easeInOut" }}
           >
-            <h2 className="text-[40px] font-semibold relative inline-block leading-3">
+            <h2 className="text-3xl md:text-[40px] font-semibold relative inline-block leading-3">
               <span className="relative">
                 <span className="absolute bottom-1 -left-[18px] w-[calc(100%+20px)] h-[24px] bg-[#FF8C5E] -z-10" />
                 {t("title1")}
@@ -66,12 +66,12 @@ const ArtSection = () => {
 
           {/* Text Block 2 */}
           <motion.div
-            className="text-start"
+            className="text-center lg:text-start"
             initial={{ opacity: 0, x: 100 }} // Start from right
             whileInView={{ opacity: 1, x: 0 }} // whileInView to original position
             transition={{ duration: 1, ease: "easeInOut" }}
           >
-            <h2 className="text-[40px] font-semibold relative inline-block">
+            <h2 className="text-3xl md:text-[40px] font-semibold relative inline-block leading-[60px]">
               <span className="relative">
                 <span className="absolute bottom-1 -left-[18px] w-[calc(100%+20px)] h-[24px] bg-[#FF8C5E] -z-10" />
                 {t("title2")}
@@ -81,7 +81,7 @@ const ArtSection = () => {
 
           {/* Paragraph */}
           <motion.p
-            className="font-normal text-xl leading-[29px] text-start mt-4"
+            className="font-normal text-lg md:text-xl leading-[29px] text-center lg:text-start mt-4"
             initial={{ opacity: 0 }} // Start with opacity 0
             whileInView={{ opacity: 1 }} // whileInView to full opacity
             transition={{ duration: 1.2, ease: "easeInOut" }}
