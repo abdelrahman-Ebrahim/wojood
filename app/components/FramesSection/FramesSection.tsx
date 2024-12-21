8930570c43e3b997f6b90242e38e592cb166b60f
abdelrahman-Ebrahim <abdelrahmanebrabima@gmail.com>
2024-12-21 16:53:30 +0200

    resp framesSection

--- app/components/FramesSection/FramesSection.tsx
+++ app/components/FramesSection/FramesSection.tsx
@@ -8,11 +8,11 @@ const FramesSection = () => {
   const t = useTranslations("Frames");
   const locale = useLocale();
   return (
-    <section className="mt-16 text-center font-rubik">
-      <div className={`flex justify-center items-center ${locale === "ar" ? "gap-[80px]" : "gap-[200px]"}`}>
+    <section className="mt-16 text-center font-rubik lg:mx-10 xl:mx-0">
+      <div className={`flex flex-col lg:flex-row justify-center items-center ${locale === "ar" ? "gap-[80px]" : "gap-[200px]"}`}>
         {/* Text Section */}
         <motion.div
-          className="flex flex-col min-w-[364px]"
+          className="flex flex-col justify-center items-center min-w-[364px]"
           initial={{ opacity: 0, x: -50, rotate: -10 }}
           whileInView={{
             opacity: 1,
@@ -28,8 +28,8 @@ const FramesSection = () => {
           }}
         >
           {/* Text Block 1 */}
-          <div className="text-start">
-            <h2 className="text-[40px] font-semibold relative inline-block leading-3">
+          <div className="text-center lg:text-start">
+            <h2 className="text-3xl md:text-[40px] font-semibold relative inline-block leading-3">
               <span className="relative">
                 <span className="absolute bottom-1 -left-[18px] w-[calc(100%+20px)] h-[24px] bg-[#3AFAFC] -z-10" />
                 {t("title1")}
@@ -38,8 +38,8 @@ const FramesSection = () => {
           </div>
 
           {/* Text Block 2 */}
-          <div className="text-start">
-            <h2 className="text-[40px] font-semibold relative inline-block">
+          <div className="text-center lg:text-start">
+            <h2 className="text-3xl md:text-[40px] font-semibold relative inline-block leading-[60px]">
               <span className="relative">
                 <span className="absolute bottom-1 -left-[18px] w-[calc(100%+20px)] h-[24px] bg-[#3AFAFC] -z-10" />
                 {t("title2")}
@@ -49,7 +49,7 @@ const FramesSection = () => {
 
           {/* Paragraph */}
           <motion.p
-            className="font-normal text-xl leading-[29px] text-start mt-4 max-w-[300px]"
+            className="font-normal text-lg md:text-xl leading-[29px] text-center lg:text-start mt-4 max-w-[300px]"
             initial={{ opacity: 0, y: 20 }}
             whileInView={{ opacity: 1, y: 0 }}
             viewport={{ once: false, amount: 0.5 }} // Trigger when 50% is in the viewport
@@ -78,6 +78,7 @@ const FramesSection = () => {
             stiffness: 120,
             damping: 30,
           }}
+          className="mx-2 md:mx-0"
         >
           <Image
             src="/mockup.png"
