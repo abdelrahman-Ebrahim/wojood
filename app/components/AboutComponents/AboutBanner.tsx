63b0435b6b647bba476adbde7e9445e33d251eb9
abdelrahman-Ebrahim <abdelrahmanebrabima@gmail.com>
2024-12-23 11:46:31 +0200

    resp about banner

--- app/components/AboutComponents/AboutBanner.tsx
+++ app/components/AboutComponents/AboutBanner.tsx
@@ -2,12 +2,14 @@ import { useTranslations } from "next-intl";
 import React from "react";
 
 const AboutBanner = () => {
-  const t  = useTranslations("AboutBanner")
+  const t = useTranslations("AboutBanner");
   return (
-    <div className="px-[51px] w-full rounded-[48px] bg-[#FFB545] mt-[104px]">
-      <p className="px-[40px] py-[20px] font-semibold text-2xl text-[#140623] text-center">
-        {t("text")}
-      </p>
+    <div className="px-[10px] lg:px-[51px]">
+      <div className="w-full rounded-full md:rounded-[48px] bg-[#FFB545] mt-[50px] md:mt-[104px]">
+        <p className="px-8 xl:px-[40px] py-[20px] font-semibold lg:text-lg xl:text-2xl text-[#140623] text-center">
+          {t("text")}
+        </p>
+      </div>
     </div>
   );
 };
--- app/components/AboutComponents/AboutHeader.tsx
+++ app/components/AboutComponents/AboutHeader.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const AboutHeader = () => {
   const t = useTranslations("AboutHeader");
   return (
-    <section className="px-[10px] md:px-[10px] lg:px-0 xl:px-[127px] text-center">
+    <section className="px-[10px] lg:px-0 xl:px-[127px] text-center">
       <h1 className="font-semibold text-[40px] text-[#390089] leading-snug">
         {t("header")}
       </h1>
