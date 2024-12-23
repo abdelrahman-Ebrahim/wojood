99591d3443b44b032f476df75e1a40bd25821513
abdelrahman-Ebrahim <abdelrahmanebrabima@gmail.com>
2024-12-23 16:31:00 +0200

    translated publish cards

--- app/components/PublishComponents/PublishCards.tsx
+++ app/components/PublishComponents/PublishCards.tsx
@@ -1,18 +1,21 @@
+import { useLocale, useTranslations } from "next-intl";
 import Image from "next/image";
 import React from "react";
 
 const PublishCards = () => {
+  const t = useTranslations("PublishCards");
+  const locale = useLocale();
+
   return (
     <section className="container mx-auto flex max-w-screen-lg flex-col items-start gap-16 px-10 py-24 md:flex-row">
       <div className="flex w-full flex-1 flex-col gap-16 md:gap-20">
         <div className="relative flex min-h-[240px] items-start gap-2 rounded-xl bg-[#5106AC]">
-          <div className="flex flex-col items-start gap-2 pt-[48px] pb-[72px] pr-[26px]">
+          <div className="flex flex-col items-start gap-2 pt-[48px] pb-[72px] ps-[26px]">
             <div className="text-lg font-semibold text-white">
-              لوحة تحكم بسيطة وسهلة الاستخدام
+              {t("card1title")}
             </div>
             <small className="inline-block text-sm text-white">
-              تمكنك من إدارة ملفك الشخصي وأعمالك الفنية، وتتبع مبيعاتك وأرباحك،
-              والوصول إلى التقارير اللازمة لفهم وتعزيز أداء محتواك على المنصة.
+              {t("card1text")}
             </small>
           </div>
           <div className="w-1/3 shrink-0">
@@ -21,17 +24,21 @@ const PublishCards = () => {
               alt="publish1"
               width={320}
               height={349}
-              className="absolute bottom-0 -left-[68px] md:-left-20 lg:-bottom-5 lg:-left-14 w-[250px] h-[250px] lg:w-[320px] lg:h-[349px]"
+              className={`absolute bottom-0 lg:-bottom-5 w-[250px] h-[250px] lg:w-[320px] lg:h-[349px] ${
+                locale === "ar"
+                  ? "lg:-end-14 md:-end-20 -end-[68px]"
+                  : "lg:-end-24 md:-end-24 -end-[80px]"
+              }`}
             />
           </div>
         </div>
         <div className="relative flex min-h-[240px] items-start gap-2 rounded-xl bg-white">
-          <div className="flex flex-col items-start gap-2 pt-[87px] pr-[32px] pb-[110px]">
+          <div className="flex flex-col items-start gap-2 pt-[87px] ps-[32px] pb-[110px]">
             <div className="text-lg font-semibold text-[#390089]">
-              تحويل شهري للمبيعات
+              {t("card2title")}
             </div>
             <small className="inline-block text-sm text-[#606060]">
-              احصل على أرباحك بشكل منتظم وتلقائي وأدر دخلك بكفاءة.
+              {t("card2text")}
             </small>
           </div>
           <div className="w-1/2 shrink-0">
@@ -40,20 +47,21 @@ const PublishCards = () => {
               alt="publish2"
               width={291}
               height={231}
-              className="absolute bottom-0 -left-10 md:-bottom-5 md:-left-12 w-[200px] h-[200px] lg:w-[291px] lg:h-[231px]"
+              className={`absolute bottom-0  md:-bottom-5 w-[200px] h-[200px] lg:w-[291px] lg:h-[231px] ${
+                locale === "ar" ? "md:-end-12 -end-10" : "lg:-end-14"
+              }`}
             />
           </div>
         </div>
       </div>
       <div className="flex w-full flex-1 flex-col gap-16 md:mt-20 md:gap-20">
         <div className="relative flex min-h-[240px] items-start gap-2 rounded-xl bg-white">
-          <div className="flex flex-col items-start gap-2 py-[87px] pr-[32px]">
+          <div className="flex flex-col items-start gap-2 py-[87px] ps-[32px]">
             <div className="text-lg font-semibold text-[#390089]">
-              وصول عالمي
+              {t("card3title")}
             </div>
             <small className="inline-block text-sm text-[#606060]">
-              وسع نطاق تأثيرك من خلال عرض أعمالك الفنية على جمهور أكبر دون أية
-              قيود جغرافية
+              {t("card3text")}
             </small>
           </div>
           <div className="w-1/2 shrink-0">
@@ -62,17 +70,17 @@ const PublishCards = () => {
               alt="publish3"
               width={328}
               height={260}
-              className="absolute bottom-0 -left-12 w-[200px] h-[200px] md:w-[250px] mdh-[250px]  md:bottom-0 lg:w-[328px] lg:h-[260px]"
+              className={`absolute bottom-0 w-[200px] h-[200px] md:w-[250px] md:h-[250px]  md:bottom-0 lg:w-[328px] lg:h-[260px] ${locale === "ar"? "-end-12" : "lg:-end-24 md:-end-16"}`}
             />
           </div>
         </div>
         <div className="relative flex min-h-[240px] items-start gap-2 rounded-xl bg-[#5106AC]">
-          <div className="flex flex-col items-start gap-2 py-[69px] pr-[10px]">
+          <div className="flex flex-col items-start gap-2 py-[69px] ps-[10px]">
             <div className="text-lg font-semibold text-white">
-            خيارات تخصيص مزدوجة
+              {t("card4title")}
             </div>
             <small className="inline-block text-sm text-white">
-            تمنحك مرونة أكبر في تقديم أعمالك الفنية وتوسيع قاعدة عملاءك.
+              {t("card4text")}
             </small>
           </div>
           <div className="w-1/2 shrink-0">
@@ -81,7 +89,7 @@ const PublishCards = () => {
               alt="publish4"
               width={270}
               height={236}
-              className="absolute bottom-0 -left-12 w-[200px] h-[200px] lg:w-[270px] lg:h-[236px]"
+              className="absolute bottom-0 -end-12 w-[200px] h-[200px] lg:w-[270px] lg:h-[236px]"
             />
           </div>
         </div>
--- app/components/PublishComponents/PublishDesc.tsx
+++ app/components/PublishComponents/PublishDesc.tsx
@@ -1,18 +1,15 @@
+import { useTranslations } from "next-intl";
 import React from "react";
 
 const PublishDesc = () => {
+  const t = useTranslations("PublishDesc");
   return (
     <section className="mt-[51px] px-3 lg:px-[277px] flex flex-col justify-center items-center text-center">
       <p className="font-normal text-[19px] mb-10">
-        وجود ليست مجرد منصةٍ وحسب بل عالمً يفيضُ فناً؛ صُمِمَ خصيصاً لتلبية
-        احتياجات الفنانين وهواة الفن ومُقتنيه. فسواءً كنت تتطلع إلى بيع المحتوى
-        الرقمي القابل للطباعة أو إلى شراء هذه الفنون الفريدة نقدم لكم في وجود
-        فنانين كنتم أو مشترين سوقاً لعرض واقتناء تشكيلة متنوعة من المحتوى الرقمي
-        والمطبوعات المختارة بعناية. تدعوكم منصة وجود لاستكشاف جمال وعمق التجربة
-        الانسانية من خلال تجربة سلسة ومثيرة عبر الإنترنت!
+        {t("text")}
       </p>
       <button className="w-[242px] h-[49px] rounded-[32px] bg-[#390089] text-white">
-        <p className="text-lg font-medium">سجل يا فنان</p>
+        <p className="text-lg font-medium">{t("button")}</p>
       </button>
     </section>
   );
