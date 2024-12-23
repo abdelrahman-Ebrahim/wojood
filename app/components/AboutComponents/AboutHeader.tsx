6976939c5648ead701ea086babef46d2e85ce199
abdelrahman-Ebrahim <abdelrahmanebrabima@gmail.com>
2024-12-23 11:33:11 +0200

    resp about header

--- app/components/AboutComponents/AboutHeader.tsx
+++ app/components/AboutComponents/AboutHeader.tsx
@@ -4,11 +4,11 @@ import React from "react";
 const AboutHeader = () => {
   const t = useTranslations("AboutHeader");
   return (
-    <section className="px-[127px] text-center">
-      <h1 className="font-semibold text-[40px] text-[#390089]">
+    <section className="px-[10px] md:px-[10px] lg:px-0 xl:px-[127px] text-center">
+      <h1 className="font-semibold text-[40px] text-[#390089] leading-snug">
         {t("header")}
       </h1>
-      <p className="mt-10 text-xl">{t("description")}</p>
+      <p className="mt-10 text-base lg:text-xl">{t("description")}</p>
     </section>
   );
 };
